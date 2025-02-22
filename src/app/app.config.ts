109611ad37e03cf81fc85d4d732ce1c5aca8f782
jeshuarcr <jeshuarcr@gmail>
2025-02-22 17:54:40 -0400

    feat(config): set app config and routes
    - Add all app providers
    - Add protected and unprotected routes
    - Add guards to routes
    - Add layouts to routes
    - Add redirections to 404 for not matching routes
    - Add "signed-in-redirect" for redirections after sign-in

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,50 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import localeEsCo from '@angular/common/locales/es-CO';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
+import {
+	provideRouter,
+	withComponentInputBinding,
+	withInMemoryScrolling,
+	withViewTransitions
+} from '@angular/router';
 
-import { routes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+import { provideHttpClient } from '@angular/common/http';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { appRoutes } from './app.routes';
+import { provideAuth } from './core/auth/auth.provider';
+import { provideIcons } from './core/icons/icons.provider';
+import { provideMediaWatcher } from './core/media-watcher/media-watcher.provider';
+import { provideUtils } from './core/utils/utils.provider';
+
+registerLocaleData(localeEsCo);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+	providers: [
+		provideZoneChangeDetection({ eventCoalescing: true }),
+		provideRouter(
+			appRoutes,
+			withComponentInputBinding(),
+			withViewTransitions(),
+			withInMemoryScrolling({ scrollPositionRestoration: 'top', anchorScrolling: 'enabled' })
+		),
+		provideAnimationsAsync(),
+		provideHttpClient(),
+		provideAuth(),
+		provideIcons(),
+		provideMediaWatcher(),
+		provideUtils(),
+		{
+			// Use the 'fill' appearance on Angular Material form fields by default
+			provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+			useValue: {
+				appearance: 'fill'
+			}
+		},
+		// Locale configuration for date pipes
+		{
+			provide: LOCALE_ID,
+			useValue: 'es-CO'
+		}
+	]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,107 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
+import { AuthGuard } from 'app/core/auth/guards/auth.guard';
+import { NoAuthGuard } from 'app/core/auth/guards/noAuth.guard';
+import { LayoutComponent } from 'app/layout/layout.component';
 
-export const routes: Routes = [];
+export const appRoutes: Route[] = [
+	// Landing page
+	{
+		path: '',
+		pathMatch: 'full',
+		component: LayoutComponent,
+		canActivate: [NoAuthGuard],
+		canActivateChild: [NoAuthGuard],
+		data: {
+			layout: 'empty'
+		},
+		children: [{ path: '', loadChildren: () => import('app/modules/landing/landing.routes') }]
+	},
+
+	// Auth routes for guests
+	{
+		path: '',
+		canActivate: [NoAuthGuard],
+		canActivateChild: [NoAuthGuard],
+		component: LayoutComponent,
+		data: {
+			layout: 'empty'
+		},
+		children: [
+			{ path: 'sign-in', loadChildren: () => import('app/modules/auth/sign-in/sign-in.routes') }
+		]
+	},
+
+	// Auth routes for authenticated users
+	{
+		path: '',
+		canActivate: [AuthGuard],
+		canActivateChild: [AuthGuard],
+		component: LayoutComponent,
+		data: {
+			layout: 'empty'
+		},
+		children: [
+			{ path: 'sign-out', loadChildren: () => import('app/modules/auth/sign-out/sign-out.routes') }
+		]
+	},
+
+	// Redirect to home after sign in
+	{
+		path: 'signed-in-redirect',
+		pathMatch: 'full',
+		redirectTo: 'home'
+	},
+
+	// Dashboards routes
+	{
+		path: '',
+		canActivate: [AuthGuard],
+		canActivateChild: [AuthGuard],
+		component: LayoutComponent,
+		children: [
+			// Home
+			{
+				path: 'home',
+				loadComponent: () => import('@app/modules/dashboard/home/pages/home/home.component')
+			},
+
+			// Player routes
+			{
+				path: 'jugador',
+				children: [
+					// {
+					// 	path: 'partidos',
+					// 	loadChildren: () => import('@app/modules/dashboard/project/project.routes')
+					// },
+					// {
+					// 	path: 'estadisticas',
+					// 	loadChildren: () => import('@app/modules/dashboard/analytics/analytics.routes')
+					// },
+					// {
+					// 	path: 'torneos',
+					// 	loadComponent: () => import('@app/modules/dashboard/tournaments/tournaments.component')
+					// }
+				]
+			},
+
+			// Organizer routes
+			{
+				path: 'organizador',
+				children: [
+					// {
+					// 	path: 'tournaments',
+					// 	loadChildren: () => import('@app/modules/dashboard/home/pages/home/home.component')
+					// }
+				]
+			},
+
+			// 404
+			{
+				path: '404-not-found',
+				pathMatch: 'full',
+				loadChildren: () => import('app/shared/pages/error/error-404/error-404.routes')
+			},
+			{ path: '**', redirectTo: '404-not-found' }
+		]
+	}
+];
