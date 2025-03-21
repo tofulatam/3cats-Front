83a02d6f3df5ab5ca31ae5f6f9293eb3fd178e81
jeshuarcr <jeshuarcr@gmail>
2025-03-21 19:37:39 -0400

    refactor(team-card): improve HTML structure and add tooltip functionality
    - Refactor HTML structure in team-card.component.html for better readability and consistency.
    - Add MatTooltipModule to enhance user experience with tooltips on team descriptions.
    - Adjust class order for styling consistency across elements.

--- src/app/modules/landing/components/team-card/team-card.component.ts
+++ src/app/modules/landing/components/team-card/team-card.component.ts
@@ -1,12 +1,13 @@
 import { DatePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { Team } from '@app/shared/interfaces/team.interface';
 
 @Component({
 	selector: 'landing-team-card',
 	templateUrl: './team-card.component.html',
-	imports: [DatePipe, MatDividerModule],
+	imports: [DatePipe, MatDividerModule, MatTooltipModule],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	host: {
 		class:
